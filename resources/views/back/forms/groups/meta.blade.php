13491009865b8c0c75e56e499ec8a992c8cb4c81
Konstantin Bukin <bukin@inetstudio.ru>
2019-03-27 03:18:19 +0300

    Update admin panel to bootstrap 4

--- resources/views/back/forms/groups/meta.blade.php
+++ resources/views/back/forms/groups/meta.blade.php
@@ -2,79 +2,73 @@
     $item = $value;
 @endphp
 
-<div class="row">
-    <div class="col-lg-12">
-        <div class="panel-group float-e-margins" id="metaAccordion">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h5 class="panel-title">
-                        <a data-toggle="collapse" data-parent="#metaAccordion" href="#collapseMeta" aria-expanded="false" class="collapsed">Мета теги</a>
-                    </h5>
-                </div>
-                <div id="collapseMeta" class="panel-collapse collapse" aria-expanded="false">
-                    <div class="panel-body">
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h5 class="panel-title">
+            <a data-toggle="collapse" data-parent="#mainAccordion" href="#collapseMeta" aria-expanded="false" class="collapsed">Мета теги</a>
+        </h5>
+    </div>
+    <div id="collapseMeta" class="collapse" aria-expanded="false">
+        <div class="panel-body">
 
-                        {!! Form::string('meta[title]', $item->getMeta('title'), [
-                            'label' => [
-                                'title' => 'Title',
-                            ],
-                            'field' => [
-                                'class' => 'form-control countable',
-                            ],
-                        ]) !!}
+            {!! Form::string('meta[title]', $item->getMeta('title'), [
+                'label' => [
+                    'title' => 'Title',
+                ],
+                'field' => [
+                    'class' => 'form-control countable',
+                ],
+            ]) !!}
 
-                        {!! Form::string('meta[description]', $item->getMeta('description'), [
-                            'label' => [
-                                'title' => 'Description',
-                            ],
-                            'field' => [
-                                'class' => 'form-control countable',
-                            ],
-                        ]) !!}
+            {!! Form::string('meta[description]', $item->getMeta('description'), [
+                'label' => [
+                    'title' => 'Description',
+                ],
+                'field' => [
+                    'class' => 'form-control countable',
+                ],
+            ]) !!}
 
-                        {!! Form::string('meta[keywords]', $item->getMeta('keywords'), [
-                            'label' => [
-                                'title' => 'Keywords',
-                            ],
-                            'field' => [
-                                'class' => 'form-control countable',
-                            ],
-                        ]) !!}
+            {!! Form::string('meta[keywords]', $item->getMeta('keywords'), [
+                'label' => [
+                    'title' => 'Keywords',
+                ],
+                'field' => [
+                    'class' => 'form-control countable',
+                ],
+            ]) !!}
 
-                        {!! Form::string('meta[canonical]', $item->getMeta('canonical'), [
-                            'label' => [
-                                'title' => 'Canonical',
-                            ],
-                            'field' => [
-                                'class' => 'form-control',
-                            ],
-                        ]) !!}
+            {!! Form::string('meta[canonical]', $item->getMeta('canonical'), [
+                'label' => [
+                    'title' => 'Canonical',
+                ],
+                'field' => [
+                    'class' => 'form-control',
+                ],
+            ]) !!}
 
-                        {!! Form::radios('meta[robots]', $item->getMeta('robots'), [
-                            'label' => [
-                                'title' => 'Индексировать',
-                            ],
-                            'radios' => [
-                                [
-                                    'label' => 'Да',
-                                    'value' => 'index, follow',
-                                    'options' => [
-                                        'class' => 'i-checks',
-                                    ],
-                                ],
-                                [
-                                    'label' => 'Нет',
-                                    'value' => 'noindex, nofollow',
-                                    'options' => [
-                                        'class' => 'i-checks',
-                                    ],
-                                ],
-                            ],
-                        ]) !!}
+            {!! Form::radios('meta[robots]', $item->getMeta('robots'), [
+                'label' => [
+                    'title' => 'Индексировать',
+                ],
+                'radios' => [
+                    [
+                        'label' => 'Да',
+                        'value' => 'index, follow',
+                        'options' => [
+                            'class' => 'i-checks',
+                        ],
+                    ],
+                    [
+                        'label' => 'Нет',
+                        'value' => 'noindex, nofollow',
+                        'options' => [
+                            'class' => 'i-checks',
+                        ],
+                    ],
+                ],
+            ]) !!}
 
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 </div>
