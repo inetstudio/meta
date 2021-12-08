64d579ff325d2d2dc0f03188be3697e5f64c0efa
Konstantin Bukin <bukin@inetstudio.ru>
2021-12-08 14:57:40 +0300

    wip

--- entities/meta/resources/views/back/forms/groups/social_meta.blade.php
+++ entities/meta/resources/views/back/forms/groups/social_meta.blade.php
@@ -24,33 +24,11 @@
                 ],
             ]) !!}
 
-            @php
-                $ogImageMedia = $item->getFirstMedia('og_image');
-            @endphp
-
-            {!! Form::crop('og_image', $ogImageMedia, [
-                'label' => [
-                    'title' => 'og:image',
-                ],
-                'image' => [
-                    'filepath' => isset($ogImageMedia) ? url($ogImageMedia->getUrl()) : '',
-                    'filename' => isset($ogImageMedia) ? $ogImageMedia->file_name : '',
-                ],
-                'crops' => [
-                    [
-                        'title' => 'Выбрать область',
-                        'name' => 'default',
-                        'ratio' => '968/475',
-                        'value' => isset($ogImageMedia) ? $ogImageMedia->getCustomProperty('crop.default') : '',
-                        'size' => [
-                            'width' => 968,
-                            'height' => 475,
-                            'type' => 'min',
-                            'description' => 'Минимальный размер области — 968x475 пикселей'
-                        ],
-                    ],
-                ],
-            ]) !!}
+            <x-inetstudio.uploads-package.uploads::fields.back.media
+                field-name="og_image"
+                label="og:image"
+                :item="$item"
+            />
 
         </div>
     </div>
